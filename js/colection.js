f3c784d47aea42da0a53188e1f3b74dffe9baca0
natalia646 <nataxash46@gmail.com>
2023-02-14 20:22:03 +0200

    detailed

--- js/colection.js
+++ js/colection.js
@@ -1,22 +1,17 @@
 let button = document.createElement("button");
 button.innerText = "Clear colection list ";
-document.body.appendChild(button);
-
 button.onclick = function () {
   localStorage.clear();
 };
+document.body.appendChild(button);
 
 let colection = localStorage.getItem("colection");
 let colectionArray = JSON.parse(colection);
-
 let allColection = document.createElement("div");
 allColection.id = 'allColect'
 for (let robot of colectionArray) {
   let div = document.createElement("div");
-
-  const classCol = document.createAttribute("class");
-  classCol.value = "toColect";
-  div.setAttributeNode(classCol);
+  div.classList.add('toColect');
 
   let h2 = document.createElement("h2");
   h2.textContent = robot.firstName + " " + robot.lastName;
--- js/detailed.js
+++ js/detailed.js
@@ -2,19 +2,16 @@ let url = new URL(location.href);
 let dataJSON = url.searchParams.get("data");
 let robot = JSON.parse(dataJSON);
 
-let div1 = document.createElement("div");
-let h1 = document.createElement("h1");
+
+const title = document.getElementById('title')
+const h1 = document.createElement("h1");
 h1.textContent = `More about ${robot.firstName} ${robot.maidenName} ${robot.lastName}`;
-const classDiv = document.createAttribute("class");
-classDiv.value = "title";
-div1.setAttributeNode(classDiv);
-div1.append(h1);
+title.append(h1);
 
-let article = document.createElement("article");
 
-let div2 = document.createElement("div"); //! first block
-let description = document.createElement("p");
-description.innerText = `
+let text = document.getElementById('text');
+let p = document.createElement("p");
+p.innerText = `
 age: ${robot.age},
 gender:  ${robot.gender},
 email: ${robot.email},
@@ -29,40 +26,35 @@ university: ${robot.university},
 company: ${robot.company.name},
 `;
 
-let contButton = document.createElement('div');
-contButton.id = 'addToColection'
+let addToColection = document.getElementById('addToColection');
 let btnCollect = document.createElement("button");
 btnCollect.onclick = function () {
   let colectRobotFromLS = localStorage.getItem("colection");
   if (colectRobotFromLS === null) {
     localStorage.setItem("colection", JSON.stringify([robot]));
   }
+  
   let colectRobotArray = JSON.parse(colectRobotFromLS);
   colectRobotArray.push(robot);
   let stringify = JSON.stringify(colectRobotArray);
   localStorage.setItem("colection", stringify);
-  console.log(colectRobotArray.includes(robot));
   
   if (colectRobotArray.includes(robot)) {
     let img = document.createElement("img");
     img.src = "img/added.svg";
     img.id = "added";
-    contButton.append(img);
+    addToColection.append(img);
   }
- 
-
-
 };
 btnCollect.innerText = "Add to collection";
-contButton.append(btnCollect)
+addToColection.append(btnCollect)
+text.append(p, addToColection)
 
-div2.append(description, contButton);
 
-let div3 = document.createElement("div"); //! second block
+let photo = document.getElementById('photo')
 let img = document.createElement("img");
 img.src = robot.image;
+photo.append(img)
+description.append(text, photo);
 
-div3.append(img);
-article.append(div2, div3);
-
-document.body.append(div1, article);
+document.body.append(description);
